7db2981b9710bb939645ddb9a2d885be1615760b
Zhmot666 <mailboxforspam@tut.by>
2024-03-14 07:10:01 +0300

    добавляем структуру файлов

---
+++ db_client.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+import sqlite3
+import psycopg2
+
+
+class DB(ABC):
+
+    @abstractmethod
+    def add_user(self):
+        pass
+
+    @abstractmethod
+    def find_user(self):
+        pass
+
+    @abstractmethod
+    def verify_user(self):
+        pass
+
+
+class SqliteDB(DB):
+    __CONNECTION = sqlite3.connect('TicTacToe.db')
+
+    def add_user(self):
+        pass
+
+    def find_user(self):
+        pass
+
+    def verify_user(self):
+        pass
+
+
+class PostgresDB(DB):
+    __CONNECTION = psycopg2.connect('TicTacToe.db')
+
+    def add_user(self):
+        pass
+
+    def find_user(self):
+        pass
+
+    def verify_user(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ main.py
@@ -0,0 +1,10 @@
+import main_window
+import db_client
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
